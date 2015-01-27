0389dfcc35491208a37cda388bc31504b64a38fa
Matthew R. Ochs <mrochs@us.ibm.com>
2015-01-26 19:10:40 -0600

    Change cflash.c to cflash_main.c; change module name from cflsh to cflash - DON'T FORGET to update environment variables as the name of the module has changed.
    Change-Id: I6d8709b322e4c16b9a38ecfb6d14eb3ef2399ab1

--- drivers/scsi/cflash/env.bash
+++ drivers/scsi/cflash/env.bash
@@ -1,7 +1,7 @@
 ###########################
 #Linux module dev env stuff
 ###########################
-export CONFIG_CFLSH=m
+export CONFIG_CFLASH=m
 export MY_KERNEL_SRC=/kernel_src/linux-3.16.0
 
 alias makemod='make -C $MY_KERNEL_SRC M=$(pwd)'
