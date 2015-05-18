33f9797558d7b79cc935cb4cb3f3b9a1835ac0fb
Michael Neuling <mikey@neuling.org>
2015-05-18 17:24:34 +1000

    cxl-memcpy: use default context for enabling device.
    Some fixes too.

--- drivers/misc/cxl-memcpy.c
+++ drivers/misc/cxl-memcpy.c
@@ -170,18 +170,13 @@ void cxl_memcpy_setup_queue(void)
  * Start the afu context.  This is calling into the generic CXL driver code
  * (except for the contents of the WED).
  */
-void cxl_memcpy_start_context(struct cxl_context *ctx)
+int cxl_memcpy_start_context(struct cxl_context *ctx)
 {
 	u64 wed;
 
 	wed = MEMCPY_WED(cxl_memcpy_queue,
 			 MEMCPY_QUEUE_SIZE/SMP_CACHE_BYTES);
-	cxl_start_context(ctx, wed, NULL);
-}
-
-void cxl_memcpy_stop_context(struct cxl_context *ctx)
-{
-	cxl_stop_context(ctx);
+	return cxl_start_context(ctx, wed, NULL);
 }
 
 /* use memcpy afu to copy write_buf[] to read_buf[] */
@@ -195,7 +190,8 @@ static int memcpy_afu(struct pci_dev *dev)
 
 	info = kzalloc(sizeof(struct cxl_memcpy_info), GFP_KERNEL);
 
-	ctx = cxl_dev_context_init(memcpy_afu_dev);
+	/* Get default context.  Can do this or create a new one */
+	ctx = cxl_get_context(memcpy_afu_dev);
 	if (!ctx)
 		return -ENOMEM;
 
@@ -240,7 +236,11 @@ static int memcpy_afu(struct pci_dev *dev)
 	smp_mb();
 
 	/* Start Context on AFU */
-	cxl_memcpy_start_context(ctx);
+	rc = cxl_memcpy_start_context(ctx);
+	if (rc) {
+		dev_err(&dev->dev, "Can't start context");
+		return rc;
+	}
 
 	/*
 	 * Wait for interrupt to complete.  We'd never do it this way in
@@ -266,7 +266,6 @@ err3:
 err2:
 	cxl_free_afu_irqs(ctx);
 err1:
-	cxl_release_context(ctx);
 	kfree(info);
 
 	return rc;
@@ -285,7 +284,7 @@ static ssize_t device_read(struct file *fp, char __user *buff, size_t length,
 	else {
 		rc = memcpy_afu(memcpy_afu_dev);
 		if (rc)
-			return -EIO;
+			return rc;
 	}
 
 	max_bytes = BUFFER_SIZE - *ppos;
