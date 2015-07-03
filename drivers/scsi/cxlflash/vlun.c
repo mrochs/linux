d96b403e2ce0cc0260236737c0df0ebf326953e4
Manoj Kumar <kumarmn@us.ibm.com>
2015-07-03 08:50:31 -0500

    clfash: write_same should be on one port
    Since the LUN ID placed is port specific.

--- drivers/scsi/cxlflash/vlun.c
+++ drivers/scsi/cxlflash/vlun.c
@@ -414,7 +414,9 @@ init_ba_exit:
  *
  * Return: 0 on success, -1 on failure
  */
-static int write_same16(struct afu *afu, struct scsi_device *sdev, u64 lba,
+static int write_same16(struct afu *afu,
+			struct scsi_device *sdev,
+			u64 lba,
 			u32 nblks)
 {
 	struct afu_cmd *cmd = NULL;
@@ -432,7 +434,7 @@ static int write_same16(struct afu *afu, struct scsi_device *sdev, u64 lba,
 			      SISL_REQ_FLAGS_SUP_UNDERRUN |
 			      SISL_REQ_FLAGS_HOST_READ);
 
-	cmd->rcb.port_sel = lun_info->port_sel;
+	cmd->rcb.port_sel = sdev->channel + 1;
 	cmd->rcb.lun_id = lun_info->lun_id[sdev->channel];
 	cmd->rcb.data_len = CMD_BUFSIZE;
 	cmd->rcb.data_ea = (u64) cmd->buf; /* Filled w/ zeros on checkout */
