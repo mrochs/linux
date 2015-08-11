a4aeca07f66b0fd78254178d1cbdb0c00646c76d
Manoj Kumar <kumarmn@us.ibm.com>
2015-08-11 14:18:53 -0500

    cflash: Add description of LXT Entry

--- drivers/scsi/cxlflash/vlun.h
+++ drivers/scsi/cxlflash/vlun.h
@@ -17,26 +17,38 @@
 
 #define MC_RHT_NMASK      16	/* in bits */
 #define MC_CHUNK_SHIFT    MC_RHT_NMASK	/* shift to go from LBA to chunk# */
-#define LXT_LUNIDX_SHIFT  8	/* LXT entry, shift for LUN index */
-#define LXT_PERM_SHIFT    4	/* LXT entry, shift for permission bits */
 
 #define HIBIT             (BITS_PER_LONG - 1)
 
 #define MAX_AUN_CLONE_CNT 0xFF
 
-/* LXT tables are allocated dynamically in groups. This is done to
-   avoid a malloc/free overhead each time the LXT has to grow
-   or shrink.
-
-   Based on the current lxt_cnt (used), it is always possible to
-   know how many are allocated (used+free). The number of allocated
-   entries is not stored anywhere.
-
-   The LXT table is re-allocated whenever it needs to cross into
-   another group.
+/* 
+ * +-------+-------+-------+-------+-------+-------+-------+---+---+
+ * | RLBA_BASE                                     |LUN_IDX| P |SEL|
+ * +-------+-------+-------+-------+-------+-------+-------+---+---+
+ *
+ * The LXT Entry contains the physical LBA where the chunk starts (RLBA_BASE).
+ * AFU ORes the low order bits from the virtual LBA (offset into the chunk) with
+ * RLBA_BASE. The result is the physical LBA to be sent to storage.
+ * The LXT Entry also contains an index to a LUN TBL and a bitmask of which
+ * outgoing (FC) * ports can be selected. The port select bit-mask is ANDed 
+ * with a global port select bit-mask maintained by the driver.
+ * In addition, it has permission bits that are ANDed with the
+ * RHT permissions to arrive at the final permissions for the chunk.
+ *
+ * LXT tables are allocated dynamically in groups. This is done to avoid 
+ * a malloc/free overhead each time the LXT has to grow or shrink.  
+ *
+ * Based on the current lxt_cnt (used), it is always possible to know 
+ * how many are allocated (used+free). The number of allocated entries is 
+ * not stored anywhere.  
+ *
+ * The LXT table is re-allocated whenever it needs to cross into another group.
 */
 #define LXT_GROUP_SIZE          8
 #define LXT_NUM_GROUPS(lxt_cnt) (((lxt_cnt) + 7)/8)	/* alloc'ed groups */
+#define LXT_LUNIDX_SHIFT  8	/* LXT entry, shift for LUN index */
+#define LXT_PERM_SHIFT    4	/* LXT entry, shift for permission bits */
 
 struct ba_lun_info {
 	u64 *lun_alloc_map;
