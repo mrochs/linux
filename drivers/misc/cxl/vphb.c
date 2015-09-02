ed2599e1e0579e8e57efc983895d747cad598f01
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2015-09-02 16:39:18 +1000

    cxl: abort cxl_pci_enable_device_hook() if PCI channel is offline
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- drivers/misc/cxl/vphb.c
+++ drivers/misc/cxl/vphb.c
@@ -48,6 +48,10 @@ static bool cxl_pci_enable_device_hook(struct pci_dev *dev)
 
 	phb = pci_bus_to_host(dev->bus);
 	afu = (struct cxl_afu *)phb->private_data;
+
+	if (!cxl_adapter_link_ok(afu->adapter))
+		return false;
+
 	set_dma_ops(&dev->dev, &dma_direct_ops);
 	set_dma_offset(&dev->dev, PAGE_OFFSET);
 
